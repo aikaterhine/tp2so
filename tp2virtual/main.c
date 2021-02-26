65fcf9d35a7419f51a5cde73407bdf0a7de66878
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2021-02-26 19:39:08 -0300

    incluindo a pilha

--- tp2virtual/main.c
+++ tp2virtual/main.c
@@ -4,12 +4,79 @@
 #include "tabela.c" // RENOMEAR
 #include "algs.c" // RENOMEAR
 
+///////////////////////////////////////////////////////////////
+//                           PILHA                           //
+///////////////////////////////////////////////////////////////
+
+struct Pilha {
+
+	int topo; /* posiÃ§Ã£o elemento topo */
+	int capa;
+	float *pElem;
+
+};
+
+void criarpilha( struct Pilha *p, int c ){
+
+   p->topo = -1;
+   p->capa = c;
+   p->pElem = (float*) malloc (c * sizeof(float));
+
+}
+int estavazia ( struct Pilha *p ){
+
+   if( p-> topo == -1 )
+
+      return 1;   // true
+
+   else
+
+      return 0;   // false
+
+}
+
+int estacheia ( struct Pilha *p ){
+
+	if (p->topo == p->capa - 1)
+
+		return 1;
+
+	else
+
+		return 0;
+
+}
+
+void empilhar ( struct Pilha *p, float v){
+
+	p->topo++;
+	p->pElem [p->topo] = v;
+
+}
+
+float desempilhar ( struct Pilha *p ){
+
+   float aux = p->pElem [p->topo];
+   p->topo--;
+   return aux;
+
+}
+
+float retornatopo ( struct Pilha *p ){
+
+   return p->pElem [p->topo];
+
+}
+
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
 //Função para validação do algorítmo recebido por parametro em main()
-int valida_entrada(){
+int valida_entrada(char* alg){
   int val;
 
-  switch (alg) {
+  switch (*alg) {
     case ("lru"):
       val = 0;
       break;
@@ -24,20 +91,97 @@ int valida_entrada(){
       break;
     default:
       val = 1;
-
+  }
   return val;
 }
 
 int main (int argc, char *argv[]){
+
+///////////////////////////////////////////////////////////////
+/////////////////////////TESTE DA PILHA///////////////////////
+/////////////////////////////////////////////////////////////
+
+/*	struct Pilha minhapilha;
+	int capacidade, op;
+	float valor;
+
+	printf( "\nCapacidade da pilha? " );
+	scanf( "%d", &capacidade );
+
+	criarpilha (&minhapilha, capacidade);
+
+	while( 1 ){ 
+
+		printf("\n1- empilhar (push)\n");
+		printf("2- desempilhar (POP)\n");
+		printf("3- Mostrar o topo \n");
+		printf("4- sair\n");
+		printf("\nopcao? ");
+		scanf("%d", &op);
+
+		switch (op){
+
+			case 1: //push
+
+				if( estacheia( &minhapilha ) == 1 )
+
+					printf("\nPILHA CHEIA! \n");
+
+				else {
+
+					printf("\nVALOR? ");
+					scanf("%f", &valor);
+					empilhar (&minhapilha, valor);
+
+				}
+				break;
+
+			case 2: //pop
+				if ( estavazia(&minhapilha) == 1 )
+
+					printf( "\nPILHA VAZIA! \n" );
+
+				else{
+
+					valor = desempilhar (&minhapilha);
+					printf ( "\n%.1f DESEMPILHADO!\n", valor );
+
+				}
+				break;
+
+			case 3: // mostrar o topo
+				if ( estavazia (&minhapilha) == 1 )
+
+					printf( "\nPILHA VAZIA!\n" );
+
+				else {
+
+					valor = retornatopo (&minhapilha);
+					printf ( "\nTOPO: %.1f\n", valor );
+
+				}
+				break;
+
+			case 4: 
+				exit(0);
+
+			default: printf( "\nOPCAO INVALIDA! \n" );
+		}
+	}
+	
+*/
+
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
 
     char *alg = argv[1];
     char *arq = argv[2];
     int tamPagina = atoi(argv[3]);
     int tamMemoriaF = atoi(argv[4]);
     int clock = 0;
-
-		return val;
-	}
 
     /*
     Abertura e controle dos parametros de entrada
