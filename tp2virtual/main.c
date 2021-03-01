06e6e90ee42fb0e92703b9c7e052975dd0d60b36
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2021-03-01 19:52:00 -0300

    implementado algoritmo fifo
    Testei com casos pequenos, falta testar os ".log" colossais deste trabalho

--- tp2virtual/main.c
+++ tp2virtual/main.c
@@ -13,7 +13,7 @@
 
 typedef struct pagina{
 	char presente;//TODO: Retirar
-	unsigned int moldura;//indice do array de quadros que corresponde a esta pagina. TODO: Renomear para "quadro"
+	int quadro;//indice do array de quadros que corresponde a esta pagina. TODO: Renomear para "quadro"
   unsigned int numero_pagina;//numero da pagina, de acordo com os bits extraidos de um endereco do .log
   unsigned int ultimo_endereco_acessado;//contem o ultimo endereco acessado desta pagina
   bool suja;//indica se a pagina esta suja ou nao
@@ -25,72 +25,13 @@ typedef struct tabela{
 	pagina *paginas;
 } tabela;
 
-typedef struct moldura{//TODO: Renomear este struct para "quadro"
+typedef struct quadro{//TODO: Renomear este struct para "quadro"
 	pagina *pagina;//TODO: Retirar isto
 	unsigned int ultimo_acesso; //Os clocks podem ser considerados como a colocação da intrução lida
 	unsigned int _carregamento; //O instante de carregamento para a memória
   bool esta_na_memoria;//indica se o quadro esta na memoria ou nao
 	char pagina_modificada;//TODO: Retirar isto
-} moldura;
-
-typedef struct memoria_processo{//TODO: Remover este struct
-	unsigned int num_entradas;
-	moldura *molduras;
-} memoria_processo;
-
-
-
-void criaTabela(tabela *tabela, int tamPagina){//TODO: Remover esta funcao
-	tabela->num_entradas = 4194304 / tamPagina; //os enderecos fornecidos sao de 32 bits, logo temos 544288kb mapeados
-    tabela->paginas = (pagina*) malloc(sizeof(pagina) * tabela->num_entradas);
-    for(int i = 0; i < tabela->num_entradas; i++){
-    	tabela->paginas[i].presente = 0;
-		tabela->paginas[i].moldura = 0;
-    }
-}
-
-void criaMemoriaProcesso(memoria_processo *memoria_processo, int tamMemoriaF){//TODO: Remover esta funcao
-	memoria_processo->num_entradas = tamMemoriaF;
-	memoria_processo->molduras = (moldura*) malloc(sizeof(moldura)*tamMemoriaF);
-	for(int i=0;i<tamMemoriaF;i++){
-		memoria_processo->molduras[i].pagina = NULL;
-		memoria_processo->molduras[i].pagina_modificada = 0;
-	}
-}
-
-///////////////////////////////////////////////////////////////
-//                         algs                             //
-/////////////////////////////////////////////////////////////
-
-//TODO: Remover esta secao
-
-int fifo_escolha(memoria_processo *memoria_processo, unsigned int clock){
-	int escolha;
-	int primeiro_acesso = clock;
-	for(int i=0; i<memoria_processo->num_entradas; i++){
-		if(memoria_processo->molduras[i]._carregamento < primeiro_acesso){
-			escolha = i;
-			primeiro_acesso = memoria_processo->molduras[i]._carregamento;
-		}
-	}
-	return escolha;
-}
-
-int lru_escolha(memoria_processo *memoria_processo, unsigned int clock){
-	int escolha;
-	int ultimo_acesso = clock;
-	for(int i=0; i<memoria_processo->num_entradas; i++){
-		if(memoria_processo->molduras[i].ultimo_acesso < ultimo_acesso){
-			escolha = i;
-			ultimo_acesso = memoria_processo->molduras[i].ultimo_acesso;
-		}
-	}
-	return escolha;
-}
-
-int random_escolha(unsigned int num_entradas){
-	return rand() % num_entradas;
-}
+} quadro;
 
 ///////////////////////////////////////////////////////////////
 //                            Lista                         //
@@ -117,7 +58,9 @@ int inserir (Fila * sequencia, Elemento * atual, pagina page){
     return -1;         
 
   novo_elemento->page.numero_pagina = page.numero_pagina;
-  novo_elemento->page.moldura = page.moldura;
+  novo_elemento->page.quadro = page.quadro;
+  novo_elemento->page.ultimo_endereco_acessado = page.ultimo_endereco_acessado;
+  novo_elemento->page.suja = page.suja;
           
   if(atual == NULL){           
     if(sequencia->tamanho == 0)             
@@ -142,7 +85,9 @@ int remover (Fila * sequencia){
   if (sequencia->tamanho == 0)           
     return -1;         
   remov_elemento = sequencia->inicio;         
-  sequencia-> inicio = sequencia->inicio->seguinte;                 
+  sequencia-> inicio = sequencia->inicio->seguinte;
+  if(remov_elemento == sequencia->fim)
+    sequencia->fim = NULL;                   
   free (remov_elemento);         
   sequencia->tamanho--;         
   return 0;       
@@ -201,7 +146,7 @@ void empilhar ( struct Pilha *p, pagina page){
 
 	p->topo++;
 	p->pElem[p->topo].numero_pagina = page.numero_pagina;
-  p->pElem[p->topo].moldura = page.moldura;
+  p->pElem[p->topo].quadro = page.quadro;
 }
 
 pagina desempilhar ( struct Pilha *p ){
@@ -386,12 +331,7 @@ int main (int argc, char *argv[]){
   }
 
   int total_paginas = tamMemoriaF / tamPagina; //numero total de paginas/quadros(a tabela de paginas tera no maximo esta quantidade de registros)
-
-  /*
-  Criar a tabela de paginas de acordo com o tamanho especificado.
-  */
-  tabela tabela;
-  criaTabela(&tabela, tamPagina);
+  printf("Numero total de paginas na memoria e na tabela: %d\n", total_paginas);
 
   Fila *tabela_fifo;              
   if ((tabela_fifo = (Fila *) malloc(sizeof(Fila))) == NULL)           
@@ -400,27 +340,18 @@ int main (int argc, char *argv[]){
   inicializacao(tabela_fifo); 
 
 
-  /*
-  Aloca memória para o processo
-  */
-  memoria_processo memoria_processo;
-  criaMemoriaProcesso(&memoria_processo, tamMemoriaF);
-
-  moldura quadros_memoria[total_paginas];//esta sera a estrutura responsavel por simular os quadros que estarao na memoria
-
+  quadro quadros_memoria[total_paginas];//esta sera a estrutura responsavel por simular os quadros que estarao na memoria
+  //inicializo atributos dos quadros
   int i;
   for(i = 0; i < total_paginas; i++){
     quadros_memoria[i].esta_na_memoria = false;
   }
 
-  /*
-  leitura dos acessos.
-  */
-  unsigned int endereco;
-  char operacao;
-  int tamPaginaBytes = tamPagina * pow(2, 10); //tamanho da página em bytes
+  unsigned int endereco;//endereco lido em uma linha do arquivo
+  char operacao;//operacao descrita em uma linha do arquivo
+  int tamPaginaBytes = tamPagina * pow(2, 10); //tamanho da pagina em bytes
 
-  //Pego o numero maximo de bits que podem ser usados para identificar as paginas 
+  //Pego o numero maximo de bits que podem ser usados para identificar as paginas(algoritmo retirado da especificacao deste trabalho) 
   unsigned s, tmp;
   tmp = tamPaginaBytes;
   s = 0;
@@ -440,68 +371,87 @@ int main (int argc, char *argv[]){
 
   printf("Executando simulador ...\n");
 
-  // imprimir a tabela de paginas aqui antes da execucao dos algoritmos
-
   clock_t inicio = clock();
 
   while(fscanf(arquivo,"%x %c\n", &endereco, &operacao) != EOF){
     contador_clock++;
-
-    printf("----------------Numero da pagina acessada: %d\n", endereco >> s);
-
-    /*Elemento* i_elemento;
-    i_elemento = tabela_fifo->inicio;
-    while(1){
-      printf("Numero da pagina na fila: %u\n", i_elemento->page.numero_pagina);
-
-      if(i_elemento == tabela_fifo->fim)
-        break;
-    }*/
-    
-    
-    /*//incrementa o número de instruções lidas (semelhante aos pulsos de clock)
-    indice = endereco % tabela.num_entradas;
-    //achei a pagina, agora vou acessar o conteudo dela
     leituras += operacao == 'R' ? 1 : 0;
     escritas += operacao == 'W' ? 1 : 0;
-    if(tabela.paginas[indice].presente){
-      hit++;
-      //pagina esta na memoria principal e seu endereço é a moldura
-      //redefinindo último acesso
-      memoria_processo.molduras[tabela.paginas[indice].moldura].ultimo_acesso = contador_clock;
-      //página modificada
-      memoria_processo.molduras[tabela.paginas[indice].moldura].pagina_modificada = operacao == 'W' ? 1 : 0;
-    }else{
-      //pagina nao esta na memoria principal
-      miss++;
-      unsigned int indice_moldura;
-      if(!strcmp("fifo", alg) || !strcmp("FIFO", alg)){
-        //fifo, me dê a posição da moldura que eu possa fazer a substituição
-        indice_moldura = fifo_escolha(&memoria_processo, contador_clock);
-      }else if(!strcmp("lru", alg) || !strcmp("LRU", alg)){
-        //lru, me dê a posição da moldura que eu possa fazer a substituição
-        indice_moldura = lru_escolha(&memoria_processo, contador_clock);
-      }else if(!strcmp("random", alg) || !strcmp("RANDOM", alg)){
-        //random, me dê a posição da moldura que eu possa fazer a substituição
-        indice_moldura = random_escolha(memoria_processo.num_entradas);
+
+    unsigned int numero_pagina_acessada = endereco >> s;
+
+    if(strcmp(alg, "fifo") == 0){
+      //verifica se pagina esta na fila
+      Elemento* i_elemento;
+      i_elemento = tabela_fifo->inicio;
+      bool pagina_esta_na_fila = false;
+      while(i_elemento != NULL){
+        //se a pagina acessada esta na tabela de paginas
+        if(i_elemento->page.numero_pagina == numero_pagina_acessada){
+          hit++;
+
+          //atualiza dados da tabela, referentes a pagina acessada
+          i_elemento->page.ultimo_endereco_acessado = endereco;
+          i_elemento->page.suja = (operacao == 'W');
+
+          pagina_esta_na_fila = true;
+          break;
+        }
+
+        i_elemento = i_elemento->seguinte;
       }
-      //a página que ocupava esta moldura não está mais presente
-      if(memoria_processo.molduras[indice_moldura].pagina){
-        memoria_processo.molduras[indice_moldura].pagina->presente = 0;
-        memoria_processo.molduras[indice_moldura].pagina_modificada = 0;
+
+      //se a pagina nao esta na fila
+      if(!pagina_esta_na_fila){
+        miss++;
+
+        //pega o indice, no array de quadros na memoria, que recebera o novo quadro ou detecta que todos os quadros na memoria estao ocupados
+        int indice_quadro_a_inserir = -1;
+        for(i = 0; i < total_paginas; i++){
+          if(!quadros_memoria[i].esta_na_memoria){
+            indice_quadro_a_inserir = i;
+            break;
+          }
+        }
+
+        //se a memoria ja estiver lotada de quadros
+        if(indice_quadro_a_inserir == -1){
+          indice_quadro_a_inserir = tabela_fifo->inicio->page.quadro;
+          remover(tabela_fifo);
+        }
+
+        //insere uma nova pagina na fila
+        pagina* page = (pagina*) malloc(sizeof(pagina));
+        page->numero_pagina = numero_pagina_acessada;
+        page->quadro = indice_quadro_a_inserir;
+        page->suja = (operacao == 'W');
+        page->ultimo_endereco_acessado = endereco;
+        inserir(tabela_fifo, tabela_fifo->fim, *page);
+
+        //atualiza um atributo do quadro que estara na memoria
+        quadros_memoria[indice_quadro_a_inserir].esta_na_memoria = true;
       }
-      //a página referenciada pelo endereço agora esta presente
-      tabela.paginas[indice].presente = 1;
-      //a moldura recebe a página e atualiza seus temporizadores
-      memoria_processo.molduras[indice_moldura].pagina = &tabela.paginas[indice];
-      memoria_processo.molduras[indice_moldura]._carregamento = contador_clock;
-      memoria_processo.molduras[indice_moldura].ultimo_acesso = contador_clock;
-    }*/
+    }
+    
   }
 
   clock_t fim = clock();
 
   double tempoExecucao = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
+  if(strcmp(alg, "fifo") == 0){
+    Elemento* i_elemento;
+    i_elemento = tabela_fifo->inicio;
+
+    printf("$$$$$$$$$$$$$$Lista de páginas na tabela(algoritmo fifo): %u\n", i_elemento->page.numero_pagina);
+    while(i_elemento != NULL){
+      printf("Numero da pagina na fila: %u\n", i_elemento->page.numero_pagina);
+
+      i_elemento = i_elemento->seguinte;
+    }
+    printf("$$$$$$$$$$$$$$Fim da listagem\n");
+  }
+  
 
   // imprimir a tabela de paginas aqui depois da execucao dos algoritmos
 
